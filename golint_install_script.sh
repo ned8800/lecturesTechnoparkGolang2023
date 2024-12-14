a21aba657d7c2f9668a7fd7e245a47f9210fe4ed
Timofey Chichikin <72801901+ned8800@users.noreply.github.com>
2024-12-14 20:46:26 +0300

    added instalation scripts

---
+++ golint_install_script.sh
@@ -0,0 +1,5 @@
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+
+golangci-lint --version
+
---
+++ install_protoc.sh
@@ -0,0 +1,3 @@
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+
